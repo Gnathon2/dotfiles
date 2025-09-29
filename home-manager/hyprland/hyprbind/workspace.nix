bbf9a5c0da7fbdeb3afe5230b2a872b9792e208f
Thomas <thomas89hawk@gmail.com>
2025-09-29 13:46:08 +0200

    workspace related binds

---
+++ home-manager/hyprland/hyprbind/workspace.nix
@@ -0,0 +1,34 @@
+{...}:
+{
+  wayland.windowManager.hyprland.settings = {
+    bind = [
+      "SUPER, code:49, togglespecialworkspace, magic"
+      "SUPER, $one, workspace, 1"
+      "SUPER, $two, workspace, 2"
+      "SUPER, $three, workspace, 3"
+      "SUPER, $foor, workspace, 4"
+      "SUPER, $five, workspace, 5"
+      "SUPER, $six, workspace, 6"
+      "SUPER, $seven, workspace, 7"
+      "SUPER, $eight, workspace, 8"
+      "SUPER, $nine, workspace, 9"
+      "SUPER, $zero, workspace, 10"
+      "SUPER, $degre, togglespecialworkspace, "
+      " $M2, code:49, movetoworkspace, special:magic"
+      " $M2, $one, movetoworkspace, 1"
+      " $M2, $two, movetoworkspace, 2"
+      " $M2, $three, movetoworkspace, 3"
+      " $M2, $foor, movetoworkspace, 4"
+      " $M2, $five, movetoworkspace, 5"
+      " $M2, $six, movetoworkspace, 6"
+      " $M2, $seven, movetoworkspace, 7"
+      " $M2, $eight, movetoworkspace, 8"
+      " $M2, $nine, movetoworkspace, 9"
+      " $M2, $zero, movetoworkspace, 10"
+      " $M2, $degre, movetoworkspace, special"
+    ];
+    gesture = [
+      "4, horizontal, workspace"
+    ];
+  };
+}
