c1f2cbf5d07cafba78426d30ee8a8be5275ea28f
Thomas <thomas89hawk@gmail.com>
2025-09-29 13:51:27 +0200

    fix

--- home-manager/hyprland/hyprbind/workspace.nix
+++ home-manager/hyprland/hyprbind/workspace.nix
@@ -2,30 +2,30 @@
 {
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "SUPER, code:49, togglespecialworkspace, magic"
-      "SUPER, $one, workspace, 1"
-      "SUPER, $two, workspace, 2"
-      "SUPER, $three, workspace, 3"
-      "SUPER, $foor, workspace, 4"
-      "SUPER, $five, workspace, 5"
-      "SUPER, $six, workspace, 6"
-      "SUPER, $seven, workspace, 7"
-      "SUPER, $eight, workspace, 8"
-      "SUPER, $nine, workspace, 9"
-      "SUPER, $zero, workspace, 10"
-      "SUPER, $degre, togglespecialworkspace, "
-      " $M2, code:49, movetoworkspace, special:magic"
-      " $M2, $one, movetoworkspace, 1"
-      " $M2, $two, movetoworkspace, 2"
-      " $M2, $three, movetoworkspace, 3"
-      " $M2, $foor, movetoworkspace, 4"
-      " $M2, $five, movetoworkspace, 5"
-      " $M2, $six, movetoworkspace, 6"
-      " $M2, $seven, movetoworkspace, 7"
-      " $M2, $eight, movetoworkspace, 8"
-      " $M2, $nine, movetoworkspace, 9"
-      " $M2, $zero, movetoworkspace, 10"
-      " $M2, $degre, movetoworkspace, special"
+      "SUPER, code:49, togglespecialworkspace, magic" #²
+      "SUPER, code:10, workspace, 1" #1
+      "SUPER, code:11, workspace, 2" 
+      "SUPER, code:12, workspace, 3"
+      "SUPER, code:13, workspace, 4"
+      "SUPER, code:14, workspace, 5"
+      "SUPER, code:15, workspace, 6"
+      "SUPER, code:16, workspace, 7"
+      "SUPER, code:17, workspace, 8"
+      "SUPER, code:18, workspace, 9" #9
+      "SUPER, code:19, workspace, 10" #0
+      "SUPER, code:20, togglespecialworkspace, " #°
+      "SUPER ALT, code:49, movetoworkspace, special:magic"
+      "SUPER ALT, code:10, movetoworkspace, 1"
+      "SUPER ALT, code:11, movetoworkspace, 2"
+      "SUPER ALT, code:12, movetoworkspace, 3"
+      "SUPER ALT, code:13, movetoworkspace, 4"
+      "SUPER ALT, code:14, movetoworkspace, 5"
+      "SUPER ALT, code:15, movetoworkspace, 6"
+      "SUPER ALT, code:16, movetoworkspace, 7"
+      "SUPER ALT, code:17, movetoworkspace, 8"
+      "SUPER ALT, code:18, movetoworkspace, 9"
+      "SUPER ALT, code:19, movetoworkspace, 10"
+      "SUPER ALT, code:20, movetoworkspace, special"
     ];
     gesture = [
       "4, horizontal, workspace"
