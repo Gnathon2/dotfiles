38d27f44035dd1d5c6f19defce93bb9d4db5150d
Thomas <thomas89hawk@gmail.com>
2025-09-30 13:45:09 +0200

    i

--- home-manager/hyprland/hyprbind/system.nix
+++ home-manager/hyprland/hyprbind/system.nix
@@ -6,6 +6,8 @@
       ",XF86AudioStop, togglespecialworkspace, music"
       ",XF86AudioPrev, exec, playerctl previous"
       ",XF86AudioNext, exec, playerctl next"
+
+      "SUPER, P, exec, hyprctl dispatch dpms toggle"
     ];
     bindo = [ # long press
       ",XF86AudioPlay, exec, playerctl stop"
@@ -16,17 +18,17 @@
       "SUPER SHIFT, L, exec, systemctl suspend"
     ];
     binde = [ # continuous
-      "XF86AudioLowerVolume,   exec, pactl set-sink-volume 0 -5%"
-      ", XF86AudioRaiseVolume,   exec, pactl set-sink-volume 0 +5%"
-      "SHIFT, XF86AudioLowerVolume,   exec, pactl set-sink-volume 0 -1%"
-      "SHIFT, XF86AudioRaiseVolume,   exec, pactl set-sink-volume 0 +1%"
+      ", XF86AudioLowerVolume, exec, pactl set-sink-volume 0 -5%"
+      ", XF86AudioRaiseVolume, exec, pactl set-sink-volume 0 +5%"
+      "SHIFT, XF86AudioLowerVolume, exec, pactl set-sink-volume 0 -1%"
+      "SHIFT, XF86AudioRaiseVolume, exec, pactl set-sink-volume 0 +1%"
     ];
 
     bindle = [
-      ",             code:232,      exec, brightnessctl set 5%-"
-      ",             code:233,        exec, brightnessctl set 5%+"
-      "SHIFT,        code:232,      exec, brightnessctl set -1"
-      "SHIFT,        code:233,        exec, brightnessctl set +1"
+      ", code:232, exec, brightnessctl set 5%-"
+      ", code:233, exec, brightnessctl set 5%+"
+      "SHIFT, code:232, exec, brightnessctl set -1"
+      "SHIFT, code:233, exec, brightnessctl set +1"
     ];
   };
 
