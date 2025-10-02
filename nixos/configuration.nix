f1f1e9b7c858a39e5cf3d11e98fb662d2b802120
Thomas <thomas89hawk@gmail.com>
2025-10-02 16:09:45 +0200

    i

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -195,8 +195,16 @@
 
   services.upower.enable = true; # pour la batterie dans hyprpanel
   
-  
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+  };
 
+  services.fprintd = {
+    enable = true;
+    # tod.enable = true;
+    # tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
+  };
   
 
   #MARK: VisUAL2
