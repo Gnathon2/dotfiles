582562c687b09d64d2658647c17f3085b65afac6
Thomas <thomas.ramillon@telecomnancy.eu>
2026-01-12 13:56:10 +0100

    disabled auto kb backlight because hyprpanel is annoying

--- home-manager/hyprland/hypridle.nix
+++ home-manager/hyprland/hypridle.nix
@@ -9,12 +9,12 @@
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
-        { # kb
-          timeout = 5;
-          on-timeout = "brightnessctl -sd platform::kbd_backlight set 0";
-          on-resume = "brightnessctl -rd platform::kbd_backlight";
-          ignore_inhibit = "true";
-        }
+        # { # kb
+        #   timeout = 5;
+        #   on-timeout = "brightnessctl -sd platform::kbd_backlight set 0";
+        #   on-resume = "brightnessctl -rd platform::kbd_backlight";
+        #   ignore_inhibit = "true";
+        # }
         { # dim
           timeout = 150;
           on-timeout = "brightnessctl -s set 1         # set monitor backlight to minimum, avoid 0 on OLED monitor.";
