c408ad92a1ff81ebc2bd50d419cd699ecc8c33c4
Thomas <thomas.ramillon@telecomnancy.eu>
2026-01-26 13:50:51 +0100

    moved catpuccin

--- home-manager/other/default.nix
+++ home-manager/other/default.nix
@@ -3,4 +3,30 @@
   imports = [
     ./ghostty.nix
   ];
+  
+  catppuccin = {
+    enable = true;
+    accent = "maroon";
+    flavor = "mocha";
+
+    # btop.enable = true;
+    # ghostty.enable = true;
+    # gtk.icon.enable = true;
+    hyprlock.enable = false;
+    vscode.profiles.default.enable = false;
+    # vscode.profiles.catppuccin = {
+    #   # can't use this because it locks .vscode/extensions/ which is no no
+    #   accent = "yellow";
+    #   settings = {
+    #     boldKeywords = true;
+    #     italicComments = true;
+    #     italicKeywords = true;
+    #     colorOverrides = {};
+    #     customUIColors = {};
+    #     workbenchMode = "default";
+    #     bracketMode = "rainbow";
+    #     extraBordersEnabled = false;
+    #   };
+    # };
+  };
 }
